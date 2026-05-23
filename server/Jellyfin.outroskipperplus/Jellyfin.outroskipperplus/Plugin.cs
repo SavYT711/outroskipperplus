f2fe3e9ab9c734c4a95d2c5b43bcc8b6e76c49d0
SavYT711 <frayedsavyt@gmail.com>
2026-05-24 01:38:01 +1000

    Commited missing files

---
+++ server/Jellyfin.outroskipperplus/Jellyfin.outroskipperplus/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using Jellyfin.Plugin.OutroSkipperPlus.Configuration;
+using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+
+namespace Jellyfin.Plugin.OutroSkipperPlus;
+
+public class Plugin : BasePlugin<PluginConfiguration>
+{
+    public override string Name => "Outro Skipper Plus";
+    public override Guid Id => Guid.Parse("a1b2c3d4-e5f6-7890-abcd-ef1234567890");
+    public override string Description => "Skips outros and previews the next episode.";
+
+    public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+        : base(applicationPaths, xmlSerializer)
+    {
+        Instance = this;
+    }
+
+    public static Plugin? Instance { get; private set; }
+}
