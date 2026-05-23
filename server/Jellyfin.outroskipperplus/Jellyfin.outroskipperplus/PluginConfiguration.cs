50d93913f9a210ceb1b430d3d13a4c05bb6cf380
SavYT711 <frayedsavyt@gmail.com>
2026-05-24 01:35:16 +1000

    Initial plugin structure with configuration

---
+++ server/Jellyfin.outroskipperplus/Jellyfin.outroskipperplus/PluginConfiguration.cs
@@ -0,0 +1,10 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace Jellyfin.Plugin.OutroSkipperPlus.Configuration;
+
+public class PluginConfiguration : BasePluginConfiguration
+{
+    public bool EnableOutroSkip { get; set; } = true;
+    public bool EnableNextEpisodePreview { get; set; } = true;
+    public int CountdownSeconds { get; set; } = 30;
+}
